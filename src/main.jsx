1d21a62a14653c6fdf7508ffddeaf28ae1118a09
Jeppe Nygaard Christensen <jeppe@basemen.co>
2025-12-19 14:37:28 +0100

    Add CSS and JavaScript to enforce portrait orientation lock

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,23 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// Lock orientation to portrait
+if (screen.orientation && screen.orientation.lock) {
+  screen.orientation.lock('portrait').catch(() => {
+    // Lock failed, orientation API might not be supported
+    console.log('Orientation lock not supported');
+  });
+} else if (screen.lockOrientation) {
+  // Legacy API
+  screen.lockOrientation('portrait');
+} else if (screen.mozLockOrientation) {
+  // Firefox
+  screen.mozLockOrientation('portrait');
+} else if (screen.msLockOrientation) {
+  // IE/Edge
+  screen.msLockOrientation('portrait');
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
